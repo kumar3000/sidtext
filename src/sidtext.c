0f7751694221a574f5d1386ee799d0fb8ae2e527
Sidarth Santhosh Kumar <sidarthoscar15@gmail.com>
2026-06-30 14:08:26 -0500

    finished implementing raw mode

--- src/sidtext.c
+++ src/sidtext.c
@@ -1,37 +1,59 @@
+/*** includes ***/
+
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <errno.h>
+
+/*** data ***/
 
 struct termios cooked;
 
+/*** terminal ***/
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &cooked);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &cooked) == -1) die("tcsetattr");
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &cooked);
+  if (tcgetattr(STDIN_FILENO, &cooked) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = cooked;
-  raw.c_iflag &= ~(ICRNL | IXON);
-  raw.c_oflag &= ~(OPOST);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  // We want every keystroke to be sent to the program as ASCII, including ctrl characters and special characters.
+  // We don't want to handle SIGINT, SIGQUIT, SIGTSTP, etc.
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q') break;
     }
 
     return 0;
