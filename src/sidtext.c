a927f207742371b2f64a2494715ffb92edfaecd5
Sidarth Santhosh Kumar <sidarthoscar15@gmail.com>
2026-06-30 13:46:49 -0500

    add: enable/disable terminal raw mode

--- src/sidtext.c
+++ src/sidtext.c
@@ -1,6 +1,35 @@
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
+
+struct termios cooked;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &cooked);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &cooked);
+  atexit(disableRawMode);
+
+  struct termios raw = cooked;
+  raw.c_lflag &= ~(ECHO | ICANON);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
