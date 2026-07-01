9105e6eb73757bf25f734e3644377d4035284fb9
Sidarth Santhosh Kumar <sidarthoscar15@gmail.com>
2026-06-30 21:25:25 -0500

    added: tildes for every row in terminal

--- src/sidtext.c
+++ src/sidtext.c
@@ -1,36 +1,42 @@
 /*** includes ***/
-
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
 
 /*** defines ***/
-
 #define CTRL_KEY(k) ((k) & 0x1f) 
 
 /*** data ***/
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios cooked;
+};
 
-struct termios cooked;
+struct editorConfig E;
 
 /*** terminal ***/
-
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 4);
+
     perror(s);
     exit(1);
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &cooked) == -1) die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.cooked) == -1) die("disableRawMode tcsetattr");
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &cooked) == -1) die("tcgetattr");
+  if (tcgetattr(STDIN_FILENO, &E.cooked) == -1) die("enableRawMode tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = cooked;
+  struct termios raw = E.cooked;
 
   // We want every keystroke to be sent to the program as ASCII, including ctrl characters and special characters.
   // We don't want to handle SIGINT, SIGQUIT, SIGTSTP, etc.
@@ -41,7 +47,7 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("enableRawMode tcsetattr");
 }
 
 char editorReadKey() {
@@ -54,24 +60,77 @@ char editorReadKey() {
     return c;
 }
 
-/*** input ***/
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+    
+    if (write(STDOUT_FILENO, "\x1b[6n]", 4) != 4) return -1;
 
+    printf("\r\n");
+    char c;
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+/*** input ***/
 void editorProcessKeypress() {
     char c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 4);
             exit(0);
             break;
     }
 }
 
+/*** output ***/
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clear screen
+    write(STDOUT_FILENO, "\x1b[H", 3); // move cursor to top-left corner
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < E.screenrows; y++) write(STDOUT_FILENO, "~\r\n", 3);
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** init ***/
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
 
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
 
