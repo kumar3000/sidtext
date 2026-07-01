b31e9bd8aa1b3857ed463b478a2462a90546a5d1
Sidarth Santhosh Kumar <sidarthoscar15@gmail.com>
2026-06-30 20:42:46 -0500

    test-branch rewrite from memory

--- src/sidtext.c
+++ src/sidtext.c
@@ -1,78 +1,66 @@
-/*** includes ***/
-
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
 
-/*** defines ***/
-
-#define CTRL_KEY(k) ((k) & 0x1f) 
-
-/*** data ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 struct termios cooked;
 
-/*** terminal ***/
-
-void die(const char *s) {
-    perror(s);
+void serror(const char *e) {
+    perror(e);
     exit(1);
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &cooked) == -1) die("tcsetattr");
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &cooked) == -1) {
+        serror("void disableRawMode() { tcsetattr }");
+    }
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &cooked) == -1) die("tcgetattr");
-  atexit(disableRawMode);
+    atexit(disableRawMode);
 
-  struct termios raw = cooked;
+    if (tcgetattr(STDIN_FILENO, &cooked) == -1) {
+        serror("void enableRawMode() { tcgetattr }");
+    }
 
-  // We want every keystroke to be sent to the program as ASCII, including ctrl characters and special characters.
-  // We don't want to handle SIGINT, SIGQUIT, SIGTSTP, etc.
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_oflag &= ~(OPOST);
-  raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+    struct termios raw = cooked;
+    
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag != (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        serror("void enableRawMode() { tcsetattr }");
+    }
 }
 
-char editorReadKey() {
-    int nread;
+char readKey() {
     char c;
-    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-        if (nread == -1 && errno != EAGAIN) die("read");
+    if (read(STDIN_FILENO, &c, 1) == -1) {
+        serror("char readKey() { read() }");
     }
 
     return c;
 }
 
-/*** input ***/
+void processKey() {
+    char c = readKey();
 
-void editorProcessKeypress() {
-    char c = editorReadKey();
-
-    switch (c) {
+    switch(c) {
         case CTRL_KEY('q'):
             exit(0);
             break;
     }
 }
 
-/*** init ***/
-
 int main() {
     enableRawMode();
 
     while (1) {
-        editorProcessKeypress();
+        processKey();
     }
 
     return 0;
---
+++ src/sidtext_backup.c
@@ -0,0 +1,79 @@
+/*** includes ***/
+
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f) 
+
+/*** data ***/
+
+struct termios cooked;
+
+/*** terminal ***/
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &cooked) == -1) die("tcsetattr");
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &cooked) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+
+  struct termios raw = cooked;
+
+  // We want every keystroke to be sent to the program as ASCII, including ctrl characters and special characters.
+  // We don't want to handle SIGINT, SIGQUIT, SIGTSTP, etc.
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+
+    return c;
+}
+
+/*** input ***/
+
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
+/*** init ***/
+
+int main() {
+    enableRawMode();
+
+    while (1) {
+        editorProcessKeypress();
+    }
+
+    return 0;
+}
